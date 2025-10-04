0760b8984d3ee47b26e58fab59715682272ea5d8
Nam Phong <phongnnhe176274@fpt.edu.vn>
2025-10-05 01:09:37 +0700

    fix error project

--- Aura_Candle/backend/server.js
+++ Aura_Candle/backend/server.js
@@ -29,14 +29,17 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 //  CORS (chỉ cần cho phép domain frontend, không cần credentials)
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",        // khi chạy local dev
-      "https://aura-candle.vercel.app" // domain deploy thật trên Vercel
-    ]
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:3000",        // khi chạy local dev
+//       "https://aura-candle.vercel.app" // domain deploy thật trên Vercel
+//     ]
+//   })
+// );
+
+app.use(cors({ origin: "*" }));
+
 
 // API routes
 app.use("/product", productRouter);
